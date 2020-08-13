03090ee385c1dd266d31a33626dd8fa6e048aee2
jayab2010 <jayab2010@gmail.com>
2020-08-13 18:32:27 +0000

    updated file

--- mongo_project.py
+++ mongo_project.py
@@ -1,7 +1,7 @@
 import pymongo
 import os
 
-MONGODB_URI ="mongodb://root:r00tUser@myfirstcluster-shard-00-00.w52aw.mongodb.net:27017,myfirstcluster-shard-00-01.w52aw.mongodb.net:27017,myfirstcluster-shard-00-02.w52aw.mongodb.net:27017/myTestDB?ssl=true&replicaSet=atlas-n79620-shard-0&authSource=admin&retryWrites=true&w=majority"
+
 DBS_NAME = "myTestDB"
 COLLECTION_NAME = "myFirstMDB"
 
